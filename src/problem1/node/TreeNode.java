12adee37a7343e0f1934ebc6fc804c8fd2097c26
98prateeksharma <prateek.sharma_cs18@gla.ac.in>
2020-03-30 14:03:17 +0530

    Constructor added:TreeNode(data)

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -11,4 +11,8 @@ public class TreeNode {
     private TreeNode leftChild;
     private TreeNode rightChild;
     private TreeNode parent;
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
 }
