f2ff976a5f694c9c0fa405602397d8e00db88d00
98prateeksharma <prateek.sharma_cs18@gla.ac.in>
2020-03-30 14:04:40 +0530

    Getter and Setter added to TreeNode class

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -15,4 +15,36 @@ public class TreeNode {
     public TreeNode(int data) {
         this.data = data;
     }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(TreeNode parent) {
+        this.parent = parent;
+    }
 }
