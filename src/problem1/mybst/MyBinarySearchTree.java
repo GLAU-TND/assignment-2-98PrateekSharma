acdd226e8af5529873fc11b089f5da24ecc92ab3
98prateeksharma <prateek.sharma_cs18@gla.ac.in>
2020-03-30 14:11:31 +0530

    method added: private void insertHelper(TreeNode current,int value)

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -12,4 +12,27 @@ import problem1.node.TreeNode;
 public class MyBinarySearchTree {
     private TreeNode root;
 
+    private void insertHelper(TreeNode current, int value) {
+        TreeNode node = new TreeNode(value);
+        if (value == current.getData()) {
+            return;
+        } else if (value < current.getData()) {
+            if (current.getLeftChild() == null) {
+                //System.out.println("New Node inserted");
+                current.setLeftChild(node);
+                node.setParent(current);
+            } else {
+                insertHelper(current.getLeftChild(), value);
+            }
+        } else {
+            if (current.getRightChild() == null) {
+                //System.out.println("New Node inserted");
+                current.setRightChild(node);
+                node.setParent(current);
+            } else {
+                insertHelper(current.getRightChild(), value);
+            }
+        }
+        return;
+    }
 }
